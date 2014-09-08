f9a15946c3182f8c80f4557f03d8b3f1b76012b6
Casey Marshall <casey.s.marshall@gmail.com>
2014-09-07 21:26:30 -0700

    Test write and parse.

---
+++ src/test/java/org/metastatic/sexp4j/test/TestWriteParse.java
@@ -0,0 +1,32 @@
+package org.metastatic.sexp4j.test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.metastatic.sexp4j.*;
+
+public class TestWriteParse {
+    @Test
+    public void testBasic() throws IOException {
+        ExpressionList list = new ExpressionList();
+        ExpressionList list1 = new ExpressionList();
+        list1.add(new Atom("value".getBytes()));
+        list1.add(new Atom("foo".getBytes()));
+        list.add(list1);
+        ExpressionList list2 = new ExpressionList();
+        list2.add(new Atom("hash".getBytes()));
+        list2.add(new Atom("0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33".getBytes()));
+        list.add(list2);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        CanonicalWriter writer = new CanonicalWriter(out);
+        writer.writeExpression(list);
+        byte[] bytes = out.toByteArray();
+        System.out.println("wrote bytes: " + new String(bytes));
+        Expression expr = new CanonicalParser(new ByteArrayInputStream(bytes)).parse();
+        Assert.assertNotNull(expr);
+        Assert.assertEquals(list, expr);
+    }
+}
